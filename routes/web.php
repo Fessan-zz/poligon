5d2693281a8f20fe2f81bd8bed13b3737c4f74dc
fessan <fessan@ya.ru>
2019-06-30 14:18:31 +0700

    lesson 17

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home'); 
+
 
 Route::group(['namespace'=>'Blog','prefix'=>'blog'], function(){
 	Route::resource('posts','PostController')->names('blog.posts');
 
 });
 
+// админка блога
+
+$groupData = [
+	'namespace' => 'Blog\Admin',
+	'prefix'    => 'admin/blog',
+];
+Route::group($groupData,function(){
+
+	$methods = ['index','edit','update','create','store',];
+	Route::resource('categories','CategoryController')
+		->only($methods)
+		->names('blog.admin.categories');
+});
+
 
 //Route::resource('rest','RestTestController')->names('restTest');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
