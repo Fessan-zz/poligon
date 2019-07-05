70cbc348c29582b5cdc8f25277cbb0702ac339bd
fessan <fessan@ya.ru>
2019-07-05 15:47:04 +0700

    lesson 3

--- app/Http/Controllers/Blog/Admin/BaseController.php
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\Blog\BaseController as GuestBaseController;
 abstract class BaseController extends GuestBaseController {
 
 	public function __construct() {
-
+		//
 	}
 }
---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Class BlogCategoryRepository
+ *
+ * @package App\Repositories
+ */
+
+class BlogPostRepository extends CoreRepository{
+	/**
+	 * @return string
+	 */
+	protected function getModelClass() {
+		// TODO: Implement getModelClass() method.
+		return Model::class;
+	}
+
+	/**
+	 * Получение списка статей для вывода в админке
+	 *
+	 * @return LengthAwarePaginator
+	 */
+	public function getAllWithPaginate(){
+
+		$columns = [
+			'id',
+			'title',
+			'slug',
+			'is_published',
+			'published_at',
+			'user_id',
+			'category_id',
+		];
+
+		$result = $this->startConditions()
+			->select($columns)
+			->orderBy('id', 'DESC')
+			//>with(['category', 'user'])
+			->wiht(
+				[
+					// Можно так
+					'category' => function ($query){
+					$query->select(['id', 'title']);
+					},
+					// или так
+					'user:id,name',
+				]
+			)
+			->paginate(25);
+
+		return $result;
+	}
+}
